cf572be9e1decbdd713feade12a865238f7c755c
MooTaning <Muhammad@trienpont.com>
2023-07-07 17:03:57 +0700

    set up pdf file

--- app/test-plan/page.jsx
+++ app/test-plan/page.jsx
@@ -1,12 +1,14 @@
 "use client"
 
-import {useState, React} from 'react';
+import {useState, React,useRef} from 'react';
 import { jsPDF } from "jspdf";
 import moment from 'moment';
+import ReactPDF, {PDFViewer, Page, Text, View, Document, StyleSheet, PDFDownloadLink } from '@react-pdf/renderer';
 
-import TestPlanForm from '@components/TestPlanForm'
+import TestPlanForm from '@components/TestPlanForm';
 const page = () => {
 
+    const pdf = useRef(null)
     const [testPlan, setTestPlan] = useState({
         testerName: '',
         projectName:'',
@@ -126,9 +128,40 @@ const page = () => {
     const createTestPlan = (e) => {
         e.preventDefault();
         setSubmitting(true);
-        formatPDF();
+       //console.log()
+        pdf.current.click()
         setSubmitting(false);
     }
+
+    const styles = StyleSheet.create({
+        page: {
+          flexDirection: 'row',
+          backgroundColor: '#E4E4E4'
+        },
+        section: {
+          margin: 10,
+          padding: 10,
+          flexGrow: 1
+        }
+      });
+      
+    const MyDocument = ({ data }) => (
+        <Document>
+          <Page size="A4" style={styles.page}>
+            <View style={styles.section}>
+              <Text>Section #1 {data.moo}</Text>
+            </View>
+            <View style={styles.section}>
+              <Text>Section #2</Text>
+            </View>
+          </Page>
+        </Document>
+      );
+
+      const App = (props) => <>
+       <PDFDownloadLink  document={<MyDocument data={props.data} />} style={{ display: "none"}} fileName="somename.pdf" >
+      {({ blob, url, loading, error }) => loading ? 'Loading document...' : <div ref={pdf}>Download now!</div>}
+    </PDFDownloadLink></>;
     
     return (
         <section
@@ -137,6 +170,7 @@ const page = () => {
             width: "100%"
           }}
         >
+            <App data={{ moo: "haram"}}/>
             <TestPlanForm
                     testPlan={testPlan}
                     setTestPlan={setTestPlan}
